b13b4c68d41bb0337668165bb7a51bad400ce6a4
Jmiccolo <jmiccolo89@gmail.com>
2021-04-27 23:01:23 -0400

    Edit Landing Page and Navbar Component for Javascript

--- theLanguageEmporium/force-app/main/default/staticresources/customJavascript.js
+++ theLanguageEmporium/force-app/main/default/staticresources/customJavascript.js
@@ -1,5 +1,10 @@
-window.onload(function(){
+window.onload = function(){
 
-   
+   const navButton = document.querySelector('#navButton');
+   const sideBar = document.querySelector('#sideBar');
+
+   navButton.addEventListener('click', function(e){
+        sideBar.classList.toggle('show');
+   })
     
-})
+}
